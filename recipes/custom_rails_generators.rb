7cf05c1fbc0e0f79ab7386971d0b5bf5038a7dbb
andrea carrozzo <jonnyjava.net@gmail.com>
2016-07-08 17:00:51 +0200

    add recipes for custom rails generators

---
+++ recipes/custom_rails_generators.rb
@@ -0,0 +1,17 @@
+stage_three do
+  say_recipe 'CUSTOM RAILS GENERATORS'
+  repo = 'https://raw.githubusercontent.com/jonnyjava/rails_building_blocks/master/'
+  copy_from_repo 'lib/templates/rails/scaffold_controller/controller.rb', repo: repo
+  copy_from_repo 'lib/templates/rspec/scaffold/controller_spec.rb', repo: repo
+  copy_from_repo 'lib/templates/rspec/scaffold/routing_spec.rb', repo: repo
+end
+
+__END__
+
+name: custom_rails_generators
+description: "Clone rails generators from repo"
+author: jonnyjava.net
+
+category: homemade
+requires: [extras]
+run_after: [extras]
