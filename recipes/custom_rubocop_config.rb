e0b68a425204293f1be3f8bfd4cdf384737efe4d
andrea carrozzo <jonnyjava.net@gmail.com>
2016-08-12 12:42:25 +0200

    add custom rubocop config recipe

---
+++ recipes/custom_rubocop_config.rb
@@ -0,0 +1,18 @@
+stage_three do
+  say_recipe 'CUSTOM RUBOCOP CONFIG'
+  repo = 'https://raw.githubusercontent.com/jonnyjava/rails_building_blocks/master/'
+  copy_from_repo '.rubocop.yml', repo: repo
+  ### GIT ###
+  git :add => '. -A'
+  git :commit => '-qm "rails_apps_composer: add custom rubocop config"'
+end
+
+__END__
+
+name: custom_rubocop_config
+description: "Clone rubocop config"
+author: jonnyjava.net
+
+category: homemade
+requires: [extras]
+run_after: [extras]
