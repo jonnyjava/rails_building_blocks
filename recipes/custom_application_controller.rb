88087814fb6303fb902ffa625fc909089b72461b
jonnyjava.net <jonnyjava.net@gmail.com>
2016-11-16 18:25:54 +0100

    add application controller recipe

---
+++ recipes/custom_application_controller.rb
@@ -0,0 +1,24 @@
+stage_three do
+  say_recipe 'OVERRIDE APPLICATION_CONTROLLER.RB'
+
+remove_file "config/application_controller.rb"
+create_file 'config/application_controller.rb' do <<-EOF
+  class ApplicationController < ActionController::Base
+    include Pundit
+    protect_from_forgery with: :exception
+  end
+EOF
+end
+
+
+end
+
+__END__
+
+name: custom_application_controller
+description: "override application_controller.rb"
+author: jonnyjava.net
+
+category: homemade
+requires: [extras]
+run_after: [extras]
