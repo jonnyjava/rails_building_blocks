7dcfc4e44c9b65208dfddc1a2e97728fc4598325
jonnyjava.net <jonnyjava.net@gmail.com>
2016-11-17 14:37:06 +0100

    complete user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
 admin = User.new
-admin.name = "Lord Dark Poldo II"
+admin.name = "Poldo"
+admin.first_surname = "de Poldis"
+admin.second_surname = " Leopoldibus II"
 admin.email = 'admin@pixel.es'
 admin.password = 'Dev321'
 admin.password_confirmation = 'Dev321'
 admin.role = 'admin'
+admin.use_of_cookies = true
+admin.confirmed_at = Date.today
+admin.confirmation_sent_at = Date.today
 admin.save!
