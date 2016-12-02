e9dc7f37c2e7ccabceef8d971d9af6e296644494
jonnyjava.net <jonnyjava.net@gmail.com>
2016-12-02 12:32:59 +0100

    fix I18n_translations.yml generator to be able to generate a controller alone

--- lib/generators/rails/frontend_extras/templates/i18n_translations.rb
+++ lib/generators/rails/frontend_extras/templates/i18n_translations.rb
@@ -1,6 +1,6 @@
 es:
   <%= singular_table_name %>:
-    one: ''
+    one: ''<% if self.shell.base.respond_to?(:attributes)%>
     other: 's'<%  self.shell.base.attributes.each do |attribute|%>
-    <%= attribute.name%>: '<%= attribute.name%>'<%end%>
+    <%= attribute.name%>: '<%= attribute.name%>'<%end%><% end %>
     ransack:
