fe013fea1e89c6bb63472d1401d42a1d7d43555d
jonnyjava.net <jonnyjava.net@gmail.com>
2017-04-20 15:02:16 +0200

    add basic user locale

--- recipes/customize_boilerplate.rb
+++ recipes/customize_boilerplate.rb
@@ -21,6 +21,7 @@ stage_three do
   copy_from_repo 'config/locales/devise.es.yml', repo: repo
   copy_from_repo 'config/locales/simple_form.en.yml', repo: repo
   copy_from_repo 'config/locales/simple_form.es.yml', repo: repo
+  copy_from_repo 'config/locales/users_es.yml', repo: repo
 
   # GENERATORS
   copy_from_repo 'lib/generators/rails/frontend_extras/templates/i18n_translations.rb', repo: repo
